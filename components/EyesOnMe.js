771ebb9a1bd6685201a0750c5dbb6e70a26a12ce
Jason <jasonkwong11@gmail.com>
2017-01-17 22:04:43 -0500

    done

---
+++ components/EyesOnMe.js
@@ -0,0 +1,29 @@
+const React = require('react');
+
+class EyesOnMe extends React.Component{
+  constructor(){
+    super();
+
+    this.good = this.good.bind(this);
+    this.hey = this.hey.bind(this);
+
+  }
+
+  good(){
+    console.log('Good!');
+  }
+
+  hey(){
+    console.log('Hey! Eyes on me!');
+  }
+
+  render(){
+    return(
+      <div>
+        <button onFocus={this.good} onBlur={this.hey}> Button </button>
+      </div>
+    );
+  }
+}
+
+module.exports = EyesOnMe;
